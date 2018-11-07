020f7e3e14b0a424838260cbae11c86a663968f0
Giulio Chiappini <855062+itsgc@users.noreply.github.com>
2018-11-07 11:03:27 -0800

    first commit

--- whichport.py
+++ whichport.py
@@ -5,12 +5,12 @@ logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
 load_contrib('lldp')
 def lldp_callback(pkt):
-	# switch = pkt[LLDPDU][LLDPDUSystemName].system_name
-	# port = pkt[LLDPDU][LLDPDUPortID].id
-	vlan = pkt[LLDPDU]
-	orgspecific = vlan[LLDPDUGenericOrganisationSpecific]
-	print orgspecific.show()
-	# print "This machine is currently on: {} port {}".format(switch, port)
+	switch = pkt[LLDPDU][LLDPDUSystemName].system_name
+	port = pkt[LLDPDU][LLDPDUPortID].id
+	# vlan = pkt[LLDPDU]
+	# orgspecific = vlan[LLDPDUGenericOrganisationSpecific]
+	# print orgspecific.show()
+	print "This machine is currently on: {} port {}".format(switch, port)
 	# pkt.display()
 
 sniff(prn=lldp_callback, iface="en7", filter="ether proto 0x88cc", store=0)
